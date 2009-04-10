3eea4f82bda230175f28ea0813d05ff8abafb5c3
fabio.dias <fabio.dias@f5cd5d4e-9d46-11dd-a40c-056478bdb088>
2009-04-10 19:52:38 +0000

    Major changes: *New method to estimate R and T when using chess calibration. A little more simple and (hopefully) robust. * Modifications in the error functions of the optimizations. Added a factor in the distortions to reduce the impact of small variations. (k1 usually is between 1 and -1, T is usually bigger than 100, we had to "normalize" the search space.

--- trunk/pose_approx.m
+++ trunk/pose_approx.m
@@ -3,6 +3,8 @@ function [R T]=pose_approx(c,x,X)
 %estimates extrinsic parameters
 % REQUIRES "TRIOD" CALIBRATION PATTERN
 
+MULT=100;
+
 if (size(x,1)~=2)
     x=x';
 end
@@ -12,59 +14,40 @@ nc.R=eye(3);
 nc.T=[0;0;1];
 nc.RT=[nc.R nc.T];
 
-disp('To use this code, always measure the points of each direction in order, from the center.');
-disp('IT WILL NOT WORK OTHERWISE!');
-
 %% estimating T3
 
 % average size of the calibration pattern - approx. square
 dx=max([max(x(1,:))-min(x(1,:)) max(x(2,:))-min(x(2,:))]);
 
-xu=projection(nc,X,1);
+nc.T(3)=fminsearch(@f_t3,1);
+nc.RT=[nc.R nc.T];
+xu=projection(nc,X,1); %updating values
 
-tol=dx/10; % 10% tolerance
-previous=Inf;
-current=f_t3;
-%T3 is bigger than 1!
-while any(any(isnan(xu)))||(abs(current-previous)>tol)
-    nc.T(3)=nc.T(3)*1.1; % 10% increase each iteration - ESTIMATION -> doesn't work properly with tol too low.
-    nc.RT=[nc.R nc.T];
-    xu=projection(nc,X,1);
-    previous=current;
-    current=f_t3;
-end
 
 
 %% Now we estimate T1 e T2
 mx=mean(x');
 
 %supposing zero
-x0=[0;0];
+x0=[nc.T(1);nc.T(2)];
 aux=fminsearch(@f_t12,x0);
 nc.T(1)=aux(1);
 nc.T(2)=aux(2);
 nc.RT=[nc.R nc.T];
 xu=projection(nc,X,1); %updating values
 
-%% finding rotation matrix R
+%% finding rotation matrix R (and adapting T)
 
-id1=find(X(:,1)==max(X(:,1))); %ids of each extremity
-id2=find(X(:,2)==max(X(:,2)));
-id3=find(X(:,3)==max(X(:,3)));
-
-tx1=calculateAngle(x(:,id1-1),x(:,id1));
-tx2=calculateAngle(x(:,id2-1),x(:,id2));
-tx3=calculateAngle(x(:,id3-1),x(:,id3));
-
-aux=fminsearch(@f_R,[0;0;0]);
+aux=fminsearch(@fcn_RT,[0;0;MULT*pi;nc.T]);
+aux(1:3)=aux(1:3)/MULT;
 nc.R=mountMatrix(aux(1),aux(2),aux(3));
-nc.RT=[nc.R nc.T];
+nc.T=aux(4:6);
+nc.RT=[nc.R aux(4:6)];
 
-R=nc.R;
-T=nc.T;
+xu=projection(nc,X,1);
+
 
 %% plotting results
-xu=projection(nc,X,1);
 pl=input('Plot the reprojected points with the ESTIMATIVE of RT? []=no ','s');
 if (~isempty(pl))
     figure,
@@ -73,43 +56,31 @@ if (~isempty(pl))
     plot(x(1,:),x(2,:),'xb');
     title('Blue = correct / Red = Reprojection results');
 end
+%returning values
+R=nc.R;
+T=nc.T;
 
 
-    %calculates de angle between two points
-    function th=calculateAngle(center,extremity)
-        extremity=extremity-center;
-        th=rad2deg(cart2pol(extremity(1),extremity(2)));
-        if (th<0)
-            th=th+360;
-        end
-        
+    function sum_error=fcn_RT(vector)
+        nc.R=mountMatrix(vector(1)/MULT,vector(2)/MULT,vector(3)/MULT);
+        nc.T=vector(4:6);
+        nc.RT=[nc.R vector(4:6)];
+        sum_error=sum(sum( (projection(nc,X,1)-x).^2));
     end
 
-    %error function for R optimization. 
-    %Tries to minimize the relative angles of each extremity wrt to the
-    %center
-    function e=f_R(vector)
-        nc.R=mountMatrix(vector(1),vector(2),vector(3));
-        nc.RT=[nc.R nc.T];
-        xu=projection(nc,X,1); 
-        txu1=calculateAngle(xu(:,id1-1),xu(:,id1));
-        txu2=calculateAngle(xu(:,id2-1),xu(:,id2));
-        txu3=calculateAngle(xu(:,id3-1),xu(:,id3));
-        e=sum(sqrt((tx1-txu1)^2+(tx2-txu2)^2+(tx3-txu3)^2));
-    end
 
     %Given three angles, returns the rotation matrix
     function R=mountMatrix(a1,a2,a3)
         R1=[1         0         0        ;...
-            0         cosd(a1)  -sind(a1);...
-            0         sind(a1)  cosd(a1) ];
+            0         cos(a1)  -sin(a1)  ;...
+            0         sin(a1)   cos(a1)  ];
         
-        R2=[cosd(a2)  0         sind(a2) ;...
-            0         1         0        ;...
-            -sind(a2) 0         cosd(a2) ];
+        R2=[ cos(a2)   0         sin(a2)  ;...
+             0         1         0        ;...
+            -sin(a2)   0         cos(a2) ];
         
-        R3=[cosd(a3)  -sind(a3) 0        ;...
-            sind(a3)  cosd(a3)  0        ;...
+        R3=[cos(a3)  -sin(a3)   0        ;...
+            sin(a3)   cos(a3)   0        ;...
             0         0         1        ];
         R=R1*R2*R3;
     end
@@ -129,7 +100,10 @@ end
     %error funtion for T3
     %Tries to minimize the 'scale' diference between the calibration and
     %reprojected points.
-    function e=f_t3
+    function e=f_t3(t3)
+        nc.T(3)=t3;
+        nc.RT=[nc.R nc.T];
+        xu=projection(nc,X,1);      
         du=max([max(xu(1,:))-min(xu(1,:)) max(xu(2,:))-min(xu(2,:))]);
         e=sum(sqrt((du-dx).^2));
     end
