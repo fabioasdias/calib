c02fd68e12b6507501f708bcb3be978a95941efe
fabio.dias@gmail.com <fabio.dias@gmail.com@f5cd5d4e-9d46-11dd-a40c-056478bdb088>
2012-11-07 05:18:38 +0000

    [1 1] pixel of error on markerTracking.m measurements! - fixed

--- etc/chess_reconstruction.m
+++ etc/chess_reconstruction.m
@@ -9,13 +9,14 @@ x_left=x_left(1:nFrames,:,:);
 x_right=x_right(1:nFrames,:,:);
 nPoints=size(x_left,3);
 
+sParam=0.1;
 for p=1:nPoints
-    figure(p),subplot(2,1,1),plot(x_left(:,1,p),x_left(:,2,p),'+r');hold on;grid on;
-    x_left(:,:,p) =csaps(1:nFrames,squeeze(x_left (:,:,p))',0.05,1:nFrames)';
-    figure(p),subplot(2,1,1),plot(x_left(:,1,p),x_left(:,2,p),'ob-');hold on;grid on;
-    figure(p),subplot(2,1,2),plot(x_right(:,1,p),x_right(:,2,p),'+r');hold on;grid on;
-    x_right(:,:,p)=csaps(1:nFrames,squeeze(x_right(:,:,p))',0.05,1:nFrames)';
-    figure(p),subplot(2,1,2),plot(x_right(:,1,p),x_right(:,2,p),'ob-');hold on;grid on;
+    %figure(p),subplot(2,1,1),plot(x_left(:,1,p),x_left(:,2,p),'+r');hold on;grid on;
+    x_left(:,:,p) =csaps(1:nFrames,squeeze(x_left (:,:,p))',sParam,1:nFrames)';
+    %figure(p),subplot(2,1,1),plot(x_left(:,1,p),x_left(:,2,p),'ob-');hold on;grid on;
+%    figure(p),subplot(2,1,2),plot(x_right(:,1,p),x_right(:,2,p),'+r');hold on;grid on;
+    x_right(:,:,p)=csaps(1:nFrames,squeeze(x_right(:,:,p))',sParam,1:nFrames)';
+%    figure(p),subplot(2,1,2),plot(x_right(:,1,p),x_right(:,2,p),'ob-');hold on;grid on;
 end
 
 X=zeros(nFrames,3,nPoints);
@@ -23,11 +24,11 @@ for f=1:nFrames
     for p=1:nPoints
         xlc=x_left(f,:,p)';
         xrc=x_right(f,:,p)';
-        X(f,:,p)=fminsearch(@fcn_opt,[0;0;0]);
+        X(f,:,p)=fminsearch(@fcn_opt,[1;1;1],optimset('MaxFunEvals',1E6));
     end
 end
 
-
+save temp
 
     function err=fcn_opt(Xt)
        xl=project_points2(Xt,[0 0 0]',[0 0 0]',fc_left,cc_left,kc_left,0);
